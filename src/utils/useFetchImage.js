2fae0c336c7b546e85f57c740ca6c1583783e32b
Leah King <74308960+dancingheart714@users.noreply.github.com>
2021-09-21 11:33:18 -0400

    Add files via upload

---
+++ src/utils/useFetchImage.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+
+const url = "//api.nasa.gov/planetary/apod?"
+const key = "api_key=XqsRdChnd0Bq8ZZYyQwDpUYhjIkKfqVXW21PM8vH"
+
+//eslint-disable-next-line
+export default function useFetchImage(count) {
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    useEffect(() => {
+        setIsLoading(true);
+        axios.get(`${url}${key}&count=${count}`)
+        .then(res => {
+            setImages([...images,...res.data])
+            setIsLoading(false);
+        })
+        .catch(err => {
+            setIsLoading(false);
+        })
+    }, [count])
+    return [images, setImages, isLoading];
+}
